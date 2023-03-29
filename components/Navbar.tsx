ee3cdaa51336b7e27dae3fb55edfd51030a29c8d
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2023-03-29 14:47:08 -0700

    cmplx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,31 +14,10 @@ import {
   Heading,
   Icon,
 } from "@chakra-ui/react";
-import { AnchorHTMLAttributes, SVGAttributes, useRef } from "react";
 import NextLink from "next/link";
-
 import { BsFillPinMapFill } from "react-icons/bs";
-const HamburgerIcon = (props: SVGAttributes<SVGSVGElement>) => (
-  <svg
-    viewBox="0 0 80 62"
-    width="1em"
-    height="1em"
-    fill="currentColor"
-    {...props}
-  >
-    <path d="M80 0H0V6H80V0Z"></path>
-    <path d="M80 28H0V34H80V28Z"></path>
-    <path d="M80 56H0V62H80V56Z"></path>
-  </svg>
-);
-
-const NavItem = (props: BoxProps & AnchorHTMLAttributes<HTMLAnchorElement>) => (
-  <Box as="a" href="#" fontSize="sm" {...props} />
-);
 
 export const Navigation = () => {
-  const nav = useDisclosure();
-  const ref = useRef<HTMLDivElement>(null);
   return (
     <>
       <Box as="header">
